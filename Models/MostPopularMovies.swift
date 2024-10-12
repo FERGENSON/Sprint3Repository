c0ad8cc8c32804a0448e248486dd067600ff4d84
Fergenson <fergenson.beats@yandex.ru>
2024-10-12 18:39:45 +0300

    Подключена загрузка фильмов из сети

---
+++ Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by macbook on 12.10.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by macbook on 12.10.2024.
+//
+
+import Foundation
